adeac1e826476766a7db0fce9482133e71f9d41d
bmalinowsky <bmalinowsky@github.com>
2013-05-25 23:35:57 +0200

    Add translator for DPT 17, Scene Number

---
+++ src/tuwien/auto/calimero/dptxlator/DPTXlatorSceneNumber.java
@@ -0,0 +1,164 @@
+/*
+    Calimero 2 - A library for KNX network access
+    Copyright (c) 2013 B. Malinowsky
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+package tuwien.auto.calimero.dptxlator;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import tuwien.auto.calimero.exception.KNXFormatException;
+import tuwien.auto.calimero.exception.KNXIllegalArgumentException;
+import tuwien.auto.calimero.log.LogLevel;
+
+/**
+ * Translator for KNX DPTs with main number 17, representing <b>Scene Number</b>.
+ * <p>
+ * The KNX data type width is 1 byte.<br>
+ * The default return value after creation is scene 0.<br>
+ * <p>
+ * In value methods expecting string items, the item might be formatted using decimal, hexadecimal,
+ * and octal numbers, distinguished by using these prefixes:
+ * <li>no prefix for decimal numeral</li>
+ * <li><code>0x</code>, <code>0X</code> or <code>#</code> for hexadecimal</li>
+ * <li><code>0</code> for octal numeral</li>
+ */
+public class DPTXlatorSceneNumber extends DPTXlator
+{
+	/**
+	 * DPT ID 17.001, Scene Number; scenes are numbered with values from <b>0</b> to <b>63</b>.
+	 * <p>
+	 */
+	public static final DPT DPT_SCENE_NUMBER = new DPT("17.001", "Scene Number", "0",
+			 "63");
+
+	private static final Map types;
+
+	static {
+		types = new HashMap();
+		types.put(DPT_SCENE_NUMBER.getID(), DPT_SCENE_NUMBER);
+	}
+
+	/**
+	 * Creates a translator for the given datapoint type.
+	 * <p>
+	 * 
+	 * @param dpt the requested datapoint type
+	 * @throws KNXFormatException on not supported or not available DPT
+	 */
+	public DPTXlatorSceneNumber(final DPT dpt) throws KNXFormatException
+	{
+		this(dpt.getID());
+	}
+
+	/**
+	 * Creates a translator for the given datapoint type ID.
+	 * <p>
+	 * 
+	 * @param dptID available implemented datapoint type ID
+	 * @throws KNXFormatException on wrong formatted or not expected (available) <code>dptID</code>
+	 */
+	public DPTXlatorSceneNumber(final String dptID) throws KNXFormatException
+	{
+		super(1);
+		setTypeID(types, dptID);
+		data = new short[1];
+	}
+
+	/* (non-Javadoc)
+	 * @see tuwien.auto.calimero.dptxlator.DPTXlator#getValue()
+	 */
+	public String getValue()
+	{
+		return fromDPT(0);
+	}
+
+	/**
+	 * Sets one new translation item, replacing any old items.
+	 * <p>
+	 * 
+	 * @param scene number, 0 &lt;= scene number &lt;= 63
+	 */
+	public final void setValue(final int scene)
+	{
+		data = new short[] { toDPT(scene) };
+	}
+
+	/**
+	 * Returns the scene number of the first translation item.
+	 * <p>
+	 * 
+	 * @return unsigned 6 Bit using type short
+	 */
+	public final short getSceneNumber()
+	{
+		return (short) (data[0] & 0x3F);
+	}
+
+	/* (non-Javadoc)
+	 * @see tuwien.auto.calimero.dptxlator.DPTXlator#getAllValues()
+	 */
+	public String[] getAllValues()
+	{
+		final String[] s = new String[data.length];
+		for (int i = 0; i < data.length; ++i)
+			s[i] = fromDPT(i);
+		return s;
+	}
+
+	/* (non-Javadoc)
+	 * @see tuwien.auto.calimero.dptxlator.DPTXlator#getSubTypes()
+	 */
+	public final Map getSubTypes()
+	{
+		return types;
+	}
+
+	/**
+	 * @return the subtypes of the 8 Bit unsigned translator type
+	 * @see DPTXlator#getSubTypesStatic()
+	 */
+	protected static Map getSubTypesStatic()
+	{
+		return types;
+	}
+
+	private String fromDPT(final int index)
+	{
+		final int scene = data[index] & 0x3F;
+		return Integer.toString(scene);
+	}
+
+	protected void toDPT(final String value, final short[] dst, final int index)
+		throws KNXFormatException
+	{
+		try {
+			dst[index] = toDPT(Short.decode(value.trim()).shortValue());
+		}
+		catch (final NumberFormatException e) {
+			throw logThrow(LogLevel.WARN, "wrong value format " + value, null, value);
+		}
+	}
+
+	private short toDPT(final int value)
+	{
+		if (value < 0 || value > 63)
+			throw new KNXIllegalArgumentException("input scene number out of range [0..63]");
+		return (short) value;
+	}
+}
---
+++ test/tuwien/auto/calimero/dptxlator/DPTXlatorSceneNumberTest.java
@@ -0,0 +1,294 @@
+/*
+    Calimero 2 - A library for KNX network access
+    Copyright (c) 2013 B. Malinowsky
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+package tuwien.auto.calimero.dptxlator;
+
+import java.util.Arrays;
+
+import junit.framework.TestCase;
+import tuwien.auto.calimero.Util;
+import tuwien.auto.calimero.exception.KNXFormatException;
+import tuwien.auto.calimero.exception.KNXIllegalArgumentException;
+import tuwien.auto.calimero.log.LogManager;
+
+/**
+ * @author B. Malinowsky
+ */
+public class DPTXlatorSceneNumberTest extends TestCase
+{
+	private DPTXlatorSceneNumber t;
+
+	// min max scenes
+	private final String min = "0";
+	private final String max = "63";
+
+	private final String invalid = "64";
+	private final String invalid2 = "-1";
+
+	private final String[] invalidStrings = { invalid, invalid2, "activate " + min, "learn " + max,
+		"act " + min, "lean" + max };
+
+	private final String value1 = "13";
+	private final String[] strings = { "63", "34", "0", "30" };
+
+	private final byte[] dataMax = { (byte) 63, };
+	private final byte[] dataValue2 = { 0, 0, (byte) 30, };
+	private final byte[] data = { (byte) 63, 34, 0, (byte) 30, };
+
+	DPT sc = DPTXlatorSceneNumber.DPT_SCENE_NUMBER;
+	DPT[] dpts = { DPTXlatorSceneNumber.DPT_SCENE_NUMBER };
+
+	/**
+	 * @param name
+	 */
+	public DPTXlatorSceneNumberTest(final String name)
+	{
+		super(name);
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception
+	{
+		super.setUp();
+		LogManager.getManager().addWriter("DPTXlator", Util.getLogWriter());
+		t = new DPTXlatorSceneNumber(sc);
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception
+	{
+		Thread.sleep(100);
+		LogManager.getManager().removeWriter("DPTXlator", Util.getLogWriter());
+		super.tearDown();
+	}
+
+	/**
+	 * Test method for
+	 * {@link tuwien.auto.calimero.dptxlator.DPTXlatorSceneNumber#setValues(java.lang.String[])}.
+	 * 
+	 * @throws KNXFormatException
+	 */
+	public final void testSetValues() throws KNXFormatException
+	{
+		t.setValues(strings);
+		assertEquals(strings.length, t.getItems());
+		Helper.assertSimilar(strings, t.getAllValues());
+
+		t.setValues(new String[0]);
+		assertEquals(strings.length, t.getItems());
+		Helper.assertSimilar(strings, t.getAllValues());
+
+		final String[] s = { value1 };
+		t.setValues(s);
+		assertEquals(s.length, t.getItems());
+		Helper.assertSimilar(s, t.getAllValues());
+
+		t.setValues(new String[] { t.getValue(), t.getValue() });
+	}
+
+	/**
+	 * Test method for {@link tuwien.auto.calimero.dptxlator.DPTXlatorSceneNumber#getAllValues()}.
+	 * 
+	 * @throws KNXFormatException
+	 */
+	public void testGetAllValues() throws KNXFormatException
+	{
+		assertEquals(1, t.getItems());
+		Helper.assertSimilar("0", t.getAllValues()[0]);
+
+		t.setData(data);
+		assertEquals(data.length, t.getItems());
+		assertEquals(t.getItems(), t.getItems());
+		Helper.assertSimilar(strings, t.getAllValues());
+
+		t.setValue(strings[0]);
+		assertEquals(1, t.getItems());
+		Helper.assertSimilar(strings[0], t.getAllValues()[0]);
+	}
+
+	/**
+	 * Test method for
+	 * {@link tuwien.auto.calimero.dptxlator.DPTXlatorSceneNumber#setValue(java.lang.String)}.
+	 * 
+	 * @throws KNXFormatException
+	 */
+	public void testSetValueString() throws KNXFormatException
+	{
+		// pre and post spaces should be accepted
+		t.setValue(" " + value1 + " ");
+		Helper.assertSimilar(value1, t.getValue());
+		final String s = t.getValue();
+		t.setValue(s);
+		Helper.assertSimilar(value1, t.getValue());
+		assertEquals(s, t.getValue());
+
+		for (int i = 0; i < invalidStrings.length; i++) {
+			try {
+				t.setValue(invalidStrings[i]);
+				fail("should not succeed: " + invalidStrings[i]);
+			}
+			catch (final Exception e) {
+				// fine
+			}
+		}
+	}
+
+	/**
+	 * Test method for {@link tuwien.auto.calimero.dptxlator.DPTXlatorSceneNumber#getValue()}.
+	 * 
+	 * @throws KNXFormatException
+	 */
+	public void testGetValue() throws KNXFormatException
+	{
+		Helper.assertSimilar("0", t.getValue());
+		t.setValues(new String[0]);
+		Helper.assertSimilar("0", t.getValue());
+		t.setValue(strings[0]);
+		Helper.assertSimilar(strings[0], t.getValue());
+		t.setData(dataValue2, 2);
+		Helper.assertSimilar(strings[3], t.getValue());
+		t.setData(data);
+		Helper.assertSimilar(max, t.getValue());
+	}
+
+	/**
+	 * Test method for
+	 * {@link tuwien.auto.calimero.dptxlator.DPTXlatorSceneNumber#setData(byte[], int)}.
+	 */
+	public void testSetDataByteArrayInt()
+	{
+		t.setData(dataMax, 0);
+		try {
+			t.setData(new byte[] {}, 0);
+			fail("should throw");
+		}
+		catch (final KNXIllegalArgumentException e) {}
+		assertTrue(Arrays.equals(dataMax, t.getData()));
+		t.setData(dataValue2, 2);
+		byte[] d = t.getData();
+		assertEquals(1, d.length);
+		assertEquals(d[0], dataValue2[2]);
+
+		final byte[] array = new byte[data.length + 1];
+		System.arraycopy(data, 0, array, 1, data.length);
+		t.setData(array, 1);
+		d = t.getData();
+		assertEquals(data.length, d.length);
+		assertTrue(Arrays.equals(data, d));
+		Helper.assertSimilar(strings, t.getAllValues());
+	}
+
+	/**
+	 * Test method for
+	 * {@link tuwien.auto.calimero.dptxlator.DPTXlatorSceneNumber#getData(byte[], int)}.
+	 * 
+	 * @throws KNXFormatException
+	 */
+	public void testGetDataByteArrayInt() throws KNXFormatException
+	{
+		assertEquals(2, t.getData(new byte[2], 1).length);
+		final byte[] empty = new byte[2];
+		assertTrue(Arrays.equals(empty, t.getData(new byte[2], 1)));
+
+		t.setData(data);
+		byte[] d = new byte[10];
+		Arrays.fill(d, (byte) 0xCC);
+		t.getData(d, 3);
+		for (int i = 0; i < 3; i++)
+			assertEquals((byte) 0xCC, d[i]);
+		for (int i = 3; i < 7; i++)
+			assertEquals(data[i - 3], d[i]);
+		for (int i = 7; i < 10; i++)
+			assertEquals((byte) 0xCC, d[i]);
+
+		for (int i = 0; i < strings.length; i++) {
+			t.setValue(strings[i]);
+			d = t.getData(new byte[2 + i], i);
+			assertEquals(data[i], d[i]);
+			assertEquals(0, d[i + 1]);
+		}
+	}
+
+	/**
+	 * Test method for {@link tuwien.auto.calimero.dptxlator.DPTXlatorSceneNumber#getSubTypes()}.
+	 */
+	public void testGetSubTypes()
+	{
+		assertEquals(dpts.length, t.getSubTypes().size());
+		t.getSubTypes().remove(dpts[0].getID());
+		assertEquals(dpts.length - 1, t.getSubTypes().size());
+		// type map is static member in translator, so add DPT again
+		t.getSubTypes().put(dpts[0].getID(), dpts[0]);
+		assertEquals(dpts.length, t.getSubTypes().size());
+	}
+
+	/**
+	 * Test method for
+	 * {@link tuwien.auto.calimero.dptxlator.DPTXlatorSceneNumber#DPTXlatorSceneNumber(
+	 * tuwien.auto.calimero.dptxlator.DPT)}.
+	 */
+	public void testDPTXlatorSceneNumberDPT()
+	{
+		Helper.checkDPTs(dpts, true);
+	}
+
+	/**
+	 * Test method for
+	 * {@link tuwien.auto.calimero.dptxlator.DPTXlatorSceneNumber#setValue(int)}.
+	 * 
+	 * @throws KNXFormatException
+	 */
+	public void testSetValueInt() throws KNXFormatException
+	{
+		t.setValue(13);
+		Helper.assertSimilar(value1, t.getValue());
+		int scene = t.getSceneNumber();
+		assertEquals(13, scene);
+
+		t.setValue(0);
+		Helper.assertSimilar(strings[2], t.getValue());
+		scene = t.getSceneNumber();
+		assertEquals(0, scene);
+
+		t.setValue(34);
+		Helper.assertSimilar(strings[1], t.getValue());
+		scene = t.getSceneNumber();
+		assertEquals(34, scene);
+
+		try {
+			t.setValue(64);
+			fail("scene number too big");
+		}
+		catch (final Exception e) {
+			// fine
+		}
+
+		try {
+			t.setValue(-1);
+			fail("scene number negative");
+		}
+		catch (final Exception e) {
+			// fine
+		}
+	}
+}
