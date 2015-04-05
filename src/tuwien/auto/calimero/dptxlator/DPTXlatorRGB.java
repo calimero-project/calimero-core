0fd600c15fff9b75c12d7ee0293dc35d27d3a8ab
bmalinowsky <bmalinowsky@github.com>
2015-04-05 17:57:49 +0200

    Provide required static access to translator subtypes

--- src/tuwien/auto/calimero/dptxlator/DPTXlatorRGB.java
+++ src/tuwien/auto/calimero/dptxlator/DPTXlatorRGB.java
@@ -1,6 +1,6 @@
 /*
     Calimero 2 - A library for KNX network access
-    Copyright (c) 2006, 2014 B. Malinowsky
+    Copyright (c) 2006, 2015 B. Malinowsky
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -133,6 +133,15 @@ public class DPTXlatorRGB extends DPTXlator {
 	 * @see tuwien.auto.calimero.dptxlator.DPTXlator#getSubTypes()
 	 */
 	public Map getSubTypes()
+	{
+		return types;
+	}
+
+	/**
+	 * @return the subtypes of the RGB translator type
+	 * @see DPTXlator#getSubTypesStatic()
+	 */
+	protected static Map getSubTypesStatic()
 	{
 		return types;
 	}
