144d7639193377258086cd13a3d255f40e9837b3
bmalinowsky <bmalinowsky@github.com>
2016-01-17 22:35:50 +0100

    Continue on error getting number of elements, access might be forbidden

--- src/tuwien/auto/calimero/mgmt/PropertyClient.java
+++ src/tuwien/auto/calimero/mgmt/PropertyClient.java
@@ -1,6 +1,6 @@
 /*
     Calimero 2 - A library for KNX network access
-    Copyright (c) 2006, 2014 B. Malinowsky
+    Copyright (c) 2006, 2016 B. Malinowsky
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -767,7 +767,12 @@ public class PropertyClient implements PropertyAccess
 		InterruptedException
 	{
 		final Description d = new Description(getObjectType(oi, true), desc);
-		d.setCurrentElements(pa.getProperty(oi, d.getPID(), 0, 1));
+		try {
+			d.setCurrentElements(pa.getProperty(oi, d.getPID(), 0, 1));
+		} catch (final KNXRemoteException e) {
+			logger.warn("failed to get current number of elements for OI " + oi + " PID " + d.getPID() + ": "
+					+ e.getMessage());
+		}
 		// workaround for PDT on local DM
 		if (local)
 			d.setPDT(-1);
