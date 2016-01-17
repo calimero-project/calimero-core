08bf505059fac85b8fdccfd194429e9064fc7420
bmalinowsky <bmalinowsky@github.com>
2016-01-17 17:28:18 +0100

    Forward original cause of error if we only got one response

--- src/tuwien/auto/calimero/mgmt/ManagementClientImpl.java
+++ src/tuwien/auto/calimero/mgmt/ManagementClientImpl.java
@@ -1,6 +1,6 @@
 /*
     Calimero 2 - A library for KNX network access
-    Copyright (c) 2006, 2015 B. Malinowsky
+    Copyright (c) 2006, 2016 B. Malinowsky
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -514,8 +514,9 @@ public class ManagementClientImpl implements ManagementClient
 				ret.add(extractPropertyElements(apdu, elements));
 			}
 			catch (final KNXRemoteException e) {
-				logger.warn("skip invalid property read response "
-						+ DataUnitBuilder.toHex(apdu, ""), e);
+				if (responses.size() == 1)
+					throw e;
+				logger.warn("skip invalid property read response " + DataUnitBuilder.toHex(apdu, ""), e);
 			}
 		}
 		return ret;
