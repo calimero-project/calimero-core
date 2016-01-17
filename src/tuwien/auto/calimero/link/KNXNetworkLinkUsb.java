ec04d5e2bedd9b8c770625137878f0f544e48386
bmalinowsky <bmalinowsky@github.com>
2016-01-17 17:32:28 +0100

    Shutdown notifier on failed link setup

--- src/tuwien/auto/calimero/link/KNXNetworkLinkUsb.java
+++ src/tuwien/auto/calimero/link/KNXNetworkLinkUsb.java
@@ -1,6 +1,6 @@
 /*
     Calimero 2 - A library for KNX network access
-    Copyright (c) 2015 B. Malinowsky
+    Copyright (c) 2015, 2016 B. Malinowsky
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -129,6 +129,7 @@ public class KNXNetworkLinkUsb extends AbstractLink
 			}
 		}
 		catch (final KNXException e) {
+			notifier.quit();
 			conn.close();
 			throw e;
 		}
