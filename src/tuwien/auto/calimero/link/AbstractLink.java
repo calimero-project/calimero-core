4264ef1a3e32d1a33c81fcf3e93cece70bb2206e
bmalinowsky <bmalinowsky@github.com>
2016-02-12 19:13:07 +0100

    Line wrap

--- src/tuwien/auto/calimero/link/AbstractLink.java
+++ src/tuwien/auto/calimero/link/AbstractLink.java
@@ -1,6 +1,6 @@
 /*
     Calimero 2 - A library for KNX network access
-    Copyright (c) 2015 B. Malinowsky
+    Copyright (c) 2015, 2016 B. Malinowsky
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -281,8 +281,7 @@ public abstract class AbstractLink implements KNXNetworkLink
 
 	public String toString()
 	{
-		return "link" + (closed ? " (closed) " : " ") + getName() + " " + medium + ", hopcount "
-				+ hopCount;
+		return "link" + (closed ? " (closed) " : " ") + getName() + " " + medium + ", hopcount " + hopCount;
 	}
 
 	/**
