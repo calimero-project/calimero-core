306edae015068a8d00b4154361b40a4b56563b80
bmalinowsky <bmalinowsky@github.com>
2016-02-12 18:56:06 +0100

    Suppress some checkstyle warnings

--- src/tuwien/auto/calimero/dptxlator/DPTXlator8BitEnum.java
+++ src/tuwien/auto/calimero/dptxlator/DPTXlator8BitEnum.java
@@ -1,6 +1,6 @@
 /*
     Calimero 2 - A library for KNX network access
-    Copyright (c) 2015 B. Malinowsky
+    Copyright (c) 2015, 2016 B. Malinowsky
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -67,6 +67,7 @@ import tuwien.auto.calimero.KNXIllegalArgumentException;
  * <dd>for octal numeral</dd>
  * </dl>
  */
+@SuppressWarnings("checkstyle:javadocvariable")
 public class DPTXlator8BitEnum extends DPTXlator
 {
 	// use a common base to store and access enumeration elements independent of specific type
--- src/tuwien/auto/calimero/serial/SerialComAdapter.java
+++ src/tuwien/auto/calimero/serial/SerialComAdapter.java
@@ -1,6 +1,6 @@
 /*
     Calimero 2 - A library for KNX network access
-    Copyright (c) 2006, 2014 B. Malinowsky
+    Copyright (c) 2006, 2016 B. Malinowsky
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -51,6 +51,7 @@ import org.slf4j.Logger;
  *
  * @author B. Malinowsky
  */
+@SuppressWarnings("checkstyle:finalparameters")
 class SerialComAdapter extends LibraryAdapter
 {
 	// Notes:
