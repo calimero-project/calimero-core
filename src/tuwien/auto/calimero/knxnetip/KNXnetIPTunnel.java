bdfc6d05c035705b773fb9ac6011b849a8121788
bmalinowsky <bmalinowsky@github.com>
2016-12-11 23:28:17 +0100

    Guard workaround by opt-in flag, comment details

--- src/tuwien/auto/calimero/knxnetip/KNXnetIPTunnel.java
+++ src/tuwien/auto/calimero/knxnetip/KNXnetIPTunnel.java
@@ -168,15 +168,20 @@ public class KNXnetIPTunnel extends ClientConnection
 			return true;
 
 		final int seq = req.getSequenceNumber();
-		// ************** Star Workaround to treat missed telegram problem
-		if (seq==((getSeqRcv()+1) & 0xFF)){
-			// ups, we are one behind, don't know why, we missed it somewhere to increment or lost it.
-			// Fix this by just following the real counter again
-			logger.error("tunneling request with larger rcv-seq " + seq + ", expected "
-					+ getSeqRcv() + " -> fix this nasty situation");
-			incSeqRcv();
-		}	
-		// ************** End Workaround to treat missed telegram problem		
+		if (seq == ((getSeqRcv() + 1) & 0xFF)) {
+			// Workaround for missed request problem (not part of the knxnet/ip tunneling spec):
+			// we missed a single request, hence, the receive sequence is one behind. If the remote
+			// endpoint didn't terminate the connection, but continues to send requests, this workaround
+			// re-syncs with the sequence of the sender.
+			final String s = System.getProperty("calimero.knxnetip.tunneling.resyncSkippedRcvSeq");
+			final boolean resync = "".equals(s) || "true".equalsIgnoreCase(s);
+			if (resync) {
+				logger.error("tunneling request with rcv-seq " + seq + ", expected " + getSeqRcv()
+						+ " -> re-sync with server (1 tunneled msg lost)");
+				incSeqRcv();
+			}
+		}
+
 		if (seq == getSeqRcv() || ((seq + 1) & 0xFF) == getSeqRcv()) {
 			final int status = h.getVersion() == KNXNETIP_VERSION_10 ? ErrorCodes.NO_ERROR
 					: ErrorCodes.VERSION_NOT_SUPPORTED;
