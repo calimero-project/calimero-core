1a513de9e283db460d33da08f8cee8ec691e2cf9
bmalinowsky <bmalinowsky@github.com>
2017-10-28 16:04:57 +0200

    Minor formatting

--- src/tuwien/auto/calimero/dptxlator/TranslatorTypes.java
+++ src/tuwien/auto/calimero/dptxlator/TranslatorTypes.java
@@ -580,7 +580,7 @@ public final class TranslatorTypes
 	 * @param mainNumber datapoint type main number, 0 &lt; mainNumber
 	 * @param subNumber datapoint type sub number selecting a particular kind of value translation; use 0 to request any
 	 *        type ID of that translator (in that case, appending the physical unit for string values is disabled)
-	 * @param data (optional) KNX datapoint type data to set in the created translator for translation
+	 * @param data (optional) KNX datapoint data to set in the created translator for translation
 	 * @return the new {@link DPTXlator} object
 	 * @throws KNXException if no matching DPT translator is available or creation failed (see
 	 *         {@link MainType#createTranslator(String)})
@@ -634,7 +634,7 @@ public final class TranslatorTypes
 	 *
 	 * @param dptId datapoint type ID, formatted as <code>&lt;main number&gt;.&lt;sub number&gt;</code> with a sub
 	 *        number &lt; 100 zero-padded to 3 digits, e.g. "1.001"
-	 * @param data (optional) KNX datapoint type data to set in the created translator for translation
+	 * @param data (optional) KNX datapoint data to set in the created translator for translation
 	 * @return the new {@link DPTXlator} object
 	 * @throws KNXException if no matching DPT translator is available or creation failed (see
 	 *         {@link MainType#createTranslator(String)})
