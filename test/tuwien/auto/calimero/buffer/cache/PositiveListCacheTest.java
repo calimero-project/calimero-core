262a6977760cf57beecaad8752e3141c438d89b1
bmalinowsky <bmalinowsky@github.com>
2018-06-13 13:12:29 +0200

    Clear cache after test

--- test/tuwien/auto/calimero/buffer/cache/PositiveListCacheTest.java
+++ test/tuwien/auto/calimero/buffer/cache/PositiveListCacheTest.java
@@ -1,6 +1,6 @@
 /*
     Calimero 2 - A library for KNX network access
-    Copyright (c) 2006, 2015 B. Malinowsky
+    Copyright (c) 2006, 2018 B. Malinowsky
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -88,6 +88,7 @@ public class PositiveListCacheTest extends TestCase
 	@Override
 	protected void tearDown() throws Exception
 	{
+		exp.clear();
 		super.tearDown();
 	}
 
