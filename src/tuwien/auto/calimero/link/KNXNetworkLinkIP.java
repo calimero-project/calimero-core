5e9af291d6d0ba811956a3cda99ed6b734c57a6d
bmalinowsky <bmalinowsky@github.com>
2019-03-20 21:50:11 +0100

    Don't try configuring link with routing

--- src/tuwien/auto/calimero/link/KNXNetworkLinkIP.java
+++ src/tuwien/auto/calimero/link/KNXNetworkLinkIP.java
@@ -1,6 +1,6 @@
 /*
     Calimero 2 - A library for KNX network access
-    Copyright (c) 2006, 2018 B. Malinowsky
+    Copyright (c) 2006, 2019 B. Malinowsky
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -227,7 +227,8 @@ public class KNXNetworkLinkIP extends AbstractLink<KNXnetIPConnection>
 		final boolean useNAT, final KNXMediumSettings settings) throws KNXException, InterruptedException
 	{
 		this(serviceMode, newConnection(serviceMode, localEP, remoteEP, useNAT), settings);
-		configureWithServerSettings(localEP, remoteEP, useNAT);
+		if (serviceMode == TUNNELING)
+			configureWithServerSettings(localEP, remoteEP, useNAT);
 	}
 
 	/**
