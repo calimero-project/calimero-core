2dbac29e539ca80636e75f84fb344bae5407c73f
bmalinowsky <bmalinowsky@github.com>
2020-04-26 15:40:03 +0200

    Lower sleep time while waiting for ldm response

--- src/tuwien/auto/calimero/mgmt/LocalDeviceManagementUsb.java
+++ src/tuwien/auto/calimero/mgmt/LocalDeviceManagementUsb.java
@@ -1,6 +1,6 @@
 /*
     Calimero 2 - A library for KNX network access
-    Copyright (c) 2015, 2019 B. Malinowsky
+    Copyright (c) 2015, 2020 B. Malinowsky
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -184,7 +184,7 @@ public class LocalDeviceManagementUsb extends LocalDeviceManagement
 			catch (final KNXInvalidResponseException e) {}
 			remaining = end - System.currentTimeMillis();
 			// TODO replace busy waiting
-			Thread.sleep(50);
+			Thread.sleep(10);
 		}
 		throw new KNXInvalidResponseException(
 				"expected service confirmation msg code 0x" + Integer.toHexString(messageCode));
