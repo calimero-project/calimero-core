9e98c3e1512ad55fea89104873d0eede11baf8a3
bmalinowsky <bmalinowsky@github.com>
2020-06-10 10:30:24 +0200

    Default to max. apdu length on knx ip

--- src/tuwien/auto/calimero/link/medium/KnxIPSettings.java
+++ src/tuwien/auto/calimero/link/medium/KnxIPSettings.java
@@ -1,6 +1,6 @@
 /*
     Calimero 2 - A library for KNX network access
-    Copyright (c) 2015, 2017 B. Malinowsky
+    Copyright (c) 2015, 2020 B. Malinowsky
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -53,6 +53,7 @@ public final class KnxIPSettings extends KNXMediumSettings
 	public KnxIPSettings(final IndividualAddress device)
 	{
 		super(device);
+		setMaxApduLength(254);
 	}
 
 	@Override
