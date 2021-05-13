dc8453fde347105cc2d770cc916c493b8618bede
bmalinowsky <bmalinowsky@github.com>
2021-05-13 10:26:39 +0200

    Always request utf-8 for xml stream writer

--- src/tuwien/auto/calimero/xml/XmlStreamWriterProxy.java
+++ src/tuwien/auto/calimero/xml/XmlStreamWriterProxy.java
@@ -1,6 +1,6 @@
 /*
     Calimero 2 - A library for KNX network access
-    Copyright (c) 2015, 2017 B. Malinowsky
+    Copyright (c) 2015, 2021 B. Malinowsky
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -40,6 +40,7 @@ import java.io.Closeable;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.Writer;
+import java.nio.charset.StandardCharsets;
 
 import javax.xml.stream.FactoryConfigurationError;
 import javax.xml.stream.XMLOutputFactory;
@@ -61,7 +62,8 @@ public final class XmlStreamWriterProxy implements XmlWriter
 	public static XmlStreamWriterProxy createXmlStreamWriter(final OutputStream stream, final Closeable onClose)
 		throws XMLStreamException, FactoryConfigurationError
 	{
-		return new XmlStreamWriterProxy(XMLOutputFactory.newInstance().createXMLStreamWriter(stream), onClose);
+		return new XmlStreamWriterProxy(XMLOutputFactory.newInstance().createXMLStreamWriter(stream,
+				StandardCharsets.UTF_8.name()), onClose);
 	}
 
 	public static XmlStreamWriterProxy createXmlStreamWriter(final Writer stream)
