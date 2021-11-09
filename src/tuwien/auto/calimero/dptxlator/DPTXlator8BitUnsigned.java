527bd10f9e33b7113e619d9685db1d28747de54d
bmalinowsky <bmalinowsky@github.com>
2021-11-09 17:40:17 +0100

    Support floating-point values for scaled dpts

--- src/tuwien/auto/calimero/dptxlator/DPTXlator8BitUnsigned.java
+++ src/tuwien/auto/calimero/dptxlator/DPTXlator8BitUnsigned.java
@@ -1,6 +1,6 @@
 /*
     Calimero 2 - A library for KNX network access
-    Copyright (c) 2006, 2020 B. Malinowsky
+    Copyright (c) 2006, 2021 B. Malinowsky
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -150,7 +150,7 @@ public class DPTXlator8BitUnsigned extends DPTXlator
 	 *         subtype dimension
 	 * @see #getType()
 	 */
-	public final void setValue(final int scaled) throws KNXFormatException
+	public final void setValue(final double scaled) throws KNXFormatException
 	{
 		data = new short[] { toDPT(scaled) };
 	}
