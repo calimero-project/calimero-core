63f317486ba1d480dac2e666d8206016234c6e6b
bmalinowsky <bmalinowsky@github.com>
2022-03-07 14:02:40 +0100

    Remove duplicate entry

--- src/tuwien/auto/calimero/dptxlator/TranslatorTypes.java
+++ src/tuwien/auto/calimero/dptxlator/TranslatorTypes.java
@@ -1,6 +1,6 @@
 /*
     Calimero 2 - A library for KNX network access
-    Copyright (c) 2006, 2021 B. Malinowsky
+    Copyright (c) 2006, 2022 B. Malinowsky
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -336,7 +336,6 @@ public final class TranslatorTypes
 			"DPTXlator2ByteFloat",
 			"DptXlator2ByteSigned",
 			"DPTXlator2ByteUnsigned",
-			"DptXlator2ByteSigned",
 			"DPTXlator3BitControlled",
 			"DPTXlator4ByteFloat",
 			"DPTXlator4ByteSigned",
