9264c21af4e70b02c6590f326388fae68809c75d
bmalinowsky <bmalinowsky@github.com>
2024-03-31 01:44:45 +0100

    Useless listener, which duplicates received frames

--- src/io/calimero/link/BcuSwitcher.java
+++ src/io/calimero/link/BcuSwitcher.java
@@ -1,6 +1,6 @@
 /*
     Calimero 2 - A library for KNX network access
-    Copyright (c) 2015, 2023 B. Malinowsky
+    Copyright (c) 2015, 2024 B. Malinowsky
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -132,7 +132,6 @@ final class BcuSwitcher<T>
 	{
 		this.c = c;
 		logger = l;
-		c.addConnectionListener(e -> setResponse(e.getFrameBytes()));
 	}
 
 	enum BcuMode {
