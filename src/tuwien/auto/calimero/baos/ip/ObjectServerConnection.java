46a6f0253c45a36742b4bfbbd7a84f65b7e38b7c
bmalinowsky <bmalinowsky@github.com>
2024-07-14 12:28:09 +0200

    Deprecate duplicate method

--- src/tuwien/auto/calimero/baos/ip/ObjectServerConnection.java
+++ src/tuwien/auto/calimero/baos/ip/ObjectServerConnection.java
@@ -1,6 +1,6 @@
 /*
     Calimero 2 - A library for KNX network access
-    Copyright (c) 2021, 2021 B. Malinowsky
+    Copyright (c) 2021, 2024 B. Malinowsky
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -153,7 +153,7 @@ class ObjectServerConnection extends ClientConnection {
 	}
 
 	@Override
-	public String getName() {
+	public String name() {
 		return "KNX IP ObjectServer " + super.name();
 	}
 
