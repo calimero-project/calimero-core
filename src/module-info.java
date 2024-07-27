2a2c2e31601b066c6a1bdc454154f88859d381e1
bmalinowsky <bmalinowsky@github.com>
2024-07-27 13:45:37 +0200

    Export internals to tools

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,6 @@
 /*
     Calimero 2 - A library for KNX network access
-    Copyright (c) 2022, 2023 B. Malinowsky
+    Copyright (c) 2022, 2024 B. Malinowsky
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -70,6 +70,7 @@ module io.calimero.core {
 	exports io.calimero.internal to
 		io.calimero.device,
 		io.calimero.server,
+		io.calimero.tools,
 		io.calimero.gui,
 		io.calimero.usb.provider.javax,
 		io.calimero.testnetwork;
