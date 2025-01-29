ec7daefaedf4ef2d61858fe3ee7999e29baee5d3
bmalinowsky <bmalinowsky@github.com>
2025-01-29 17:30:45 +0100

    Ignore lint warning [try] for test classes

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,6 +57,10 @@ tasks.named<JavaCompile>("compileJava") {
 	options.javaModuleVersion = project.version.toString()
 }
 
+tasks.named<JavaCompile>("compileTestJava") {
+	options.compilerArgs.add("-Xlint:-try")
+}
+
 application {
 	mainClass.set("tuwien.auto.calimero.Settings")
 }
