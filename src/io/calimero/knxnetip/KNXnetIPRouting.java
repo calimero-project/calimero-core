95f2b613c9135a977cd78b8ff6df56773fa538a8
bmalinowsky <bmalinowsky@github.com>
2025-10-19 15:20:12 +0200

    Move logger init after setting outgoing netif

--- src/io/calimero/knxnetip/KNXnetIPRouting.java
+++ src/io/calimero/knxnetip/KNXnetIPRouting.java
@@ -381,7 +381,6 @@ public class KNXnetIPRouting extends ConnectionBase
 		try {
 			dc = newChannel();
 			dcSysBcast = !multicast.equals(systemBroadcast) ? newChannel() : null;
-			logger = LogService.getLogger("io.calimero.knxnetip." + name());
 
 			var setNetif = netIf;
 			if (setNetif != null) {
@@ -392,6 +391,7 @@ public class KNXnetIPRouting extends ConnectionBase
 			else
 				setNetif = Net.defaultNetif();
 
+			logger = LogService.getLogger("io.calimero.knxnetip." + name());
 			logger.log(DEBUG, "join multicast group {0} on {1}", multicast.getHostAddress(), setNetif.getName());
 			dc.join(multicast, setNetif);
 			if (dcSysBcast != null)
